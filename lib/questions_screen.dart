afe1d8258d7c1de593ce67849fed5258a416236b
mmJDV <mmount@jdvproducts.com>
2024-07-16 16:39:47 -0400

    mapping answers

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget{
   const QuestionsScreen({super.key});
@@ -13,33 +14,24 @@ class QuestionsScreen extends StatefulWidget{
 
 class _QuestionsScreenState extends State<QuestionsScreen>{
   Widget build(context){
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity, //wide as possible
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('The question...', 
-          style: TextStyle(
-            color: Colors.amber
+          Text(
+            currentQuestion.text, 
+            style: TextStyle(
+              color: Colors.amber
             ),
           ),
           const SizedBox(height: 30),
-          AnswerButton(
-            answerText: 'Answer 1',
-             onTap: () {}
-            ),
-          AnswerButton(
-            answerText: 'Answer 2',
-             onTap: () {}
-            ),
-          AnswerButton(
-            answerText: 'Answer 3',
-             onTap: () {}
-            ),
-          AnswerButton(
-            answerText: 'Answer 4',
-             onTap: () {}
-            ),
+          // ... adds multiple individual values to already created lists [[1,2,3],4] -> [1,2,3,4]
+          ...currentQuestion.answers.map((answer){ //map() yields an Iterable (list)
+            return AnswerButton(answerText: answer, onTap:() {});
+          })
        ],
       ),
     );
