57889d884848c90c19ec42dfcf131d02c6b2343b
mmJDV <mmount@jdvproducts.com>
2024-10-16 10:11:17 -0400

    errors on the result page

--- lib/questions_summary/questions_summary.dart
+++ lib/questions_summary/questions_summary.dart
@@ -3,19 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:adv_basics/questions_summary/summary_item.dart';
 
 class QuestionsSummary extends StatelessWidget {
-  //accept and set summary data
   const QuestionsSummary(this.summaryData, {super.key});
 
-  //summary from results screen, questions and correctness
   final List<Map<String, Object>> summaryData;
 
   @override
   Widget build(BuildContext context) {
-    //col for each question answer set
-    //children takes list, but summary is already list
     return SizedBox(
-      height: 300,
-      child: SingleChildScrollView( //defined parameters, excess is scrollable
+      height: 400,
+      child: SingleChildScrollView(
         child: Column(
           children: summaryData.map(
             (data) {
